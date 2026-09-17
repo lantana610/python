74fc54fddbe123153bbb09174ccaecd88ffc23b2
lantana610 <lantanayusuf9@gmail.com>
2026-09-17 16:13:07 +0100

    python

---
+++ break_continue.py
@@ -0,0 +1,12 @@
+name = ''
+
+while name != 'your name':
+    print('please enter your name.')
+    name = input()
+    print('thank you!')
+while True:
+    print('please type your name.')
+    name = input()
+    if name == 'your name':
+        break
+print('thank you!')        
