749e3a279e0057e8319d08a1aee8ef29907dc23b
lantana610 <lantanayusuf9@gmail.com>
2026-09-18 10:16:29 +0100

    helloFunc

---
+++ helloFunc.py
@@ -0,0 +1,12 @@
+def hello():
+    print('Howdy!')
+    print('Howdy!!!')
+    print('Hello there.')
+hello()
+hello()
+hello()
+
+def hello(name):
+     print('Hello ' + name)
+hello('Alice')
+hello('Bob')
